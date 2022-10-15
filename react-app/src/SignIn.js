40cbcf7ea0f729b1f75510a3392eeae29f372fbd
batyr0601 <batyr0601@gmail.com>
2022-10-15 17:43:33 -0500

    Hidden component

---
+++ react-app/src/SignIn.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import Todo from './components/Todo';
+import { db } from './firebase.js';
+import { collection , query, orderBy , onSnapshot, addDoc,serverTimestamp} from 'firebase/firestore';
+import './App.css';
+
+const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
+function SignIn() {
+  return (
+    <h2> signin</h2>
+  )
+}
+
+export default SignIn;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>,
-document.getElementById('root')
-);
+import SignIn from './SignIn'
+
+var right = false;
+if (right) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+} else {
+    ReactDOM.render(
+        <React.StrictMode>
+            <SignIn />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
